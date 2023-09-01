8f777b9af5912d3deaa3c630453365f6405807b9
Flakco <flacobauhaus@gmail.com>
2023-09-01 18:09:24 -0300

    Python solved exercises

---
+++ add_html.py
@@ -0,0 +1,6 @@
+def add_html(str1, tag):
+
+    print("<%s>%s</%s>" % (tag, str1, tag))
+
+
+add_html("titulo","h1")
---
+++ sort_words.py
@@ -0,0 +1,5 @@
+sentence = input("ingrese una frase con las palabras separadas por coma")
+
+separate = sentence.split(",")
+
+print(sorted(separate))
