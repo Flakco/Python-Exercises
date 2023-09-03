46e0ebe1f8efc4671252a3cbcab67fc04e07cb9f
Flakco <flacobauhaus@gmail.com>
2023-09-03 00:00:01 -0300

    Python solved exercises

--- first_upper.py
+++ first_upper.py
@@ -1,6 +1,12 @@
 def first_upper(str1):
+    count = 0
     for i in str1[:4]:
+       if i.isupper():
+           count += 1
        print(i)
+    
+    if count == 2:
+        print(str1.upper())
 
 
-first_upper()
+first_upper("MF doom")
