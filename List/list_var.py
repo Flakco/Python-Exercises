0b963dd5233019724c59fab433f1876ca2ea3440
Flakco <flacobauhaus@gmail.com>
2023-09-21 23:10:40 -0300

    Python solved exercises

---
+++ List/list_var.py
@@ -0,0 +1,8 @@
+color = [("Dark", "#000000", "rgb(0, 0, 0)"), ("Blood", "#FF0000", "rgb(255, 0, 0)"),
+         ("Sun", "#FFFF00", "rgb(255, 255, 0)")]
+
+var1, var2, var3 = color
+
+print(var1)
+print(var2)
+print(var3)
